6efae692acd032c11c74675461348ba9d20e2360
Vladimir <shumaevwork@gmail.com>
2019-07-07 18:54:25 +0400

    second commit

--- src/main/java/net/proselyte/bookmanager/service/AutoServiceImpl.java
+++ src/main/java/net/proselyte/bookmanager/service/AutoServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class AutoServiceImpl implements AutoService {
 
     AutoDao autoDao;
+    
 
     public void setAutoDao(AutoDao autoDao) {
         this.autoDao = autoDao;
